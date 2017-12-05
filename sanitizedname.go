ee1d30b2d97dcf57aa6b875f25ca89fb3d5a172b
billgo <cocobill@vip.qq.com>
2017-12-05 15:59:46 +0800

    udpate

---
+++ sanitizedname.go
@@ -0,0 +1,22 @@
+package goorgeous
+
+import "unicode"
+
+// Create returns a sanitized anchor name for the given text.
+func SanitizedNameCreate(text string) string {
+	var anchorName []rune
+	var futureDash = false
+	for _, r := range text {
+		switch {
+		case unicode.IsLetter(r) || unicode.IsNumber(r):
+			if futureDash && len(anchorName) > 0 {
+				anchorName = append(anchorName, '-')
+			}
+			futureDash = false
+			anchorName = append(anchorName, unicode.ToLower(r))
+		default:
+			futureDash = true
+		}
+	}
+	return string(anchorName)
+}
